a1771eb983e8e5aaa18de53e3e888187794a7009
Aldijana Zahirović <aldijanazahirovic406@gmail.com>
2022-07-22 21:14:25 -0700

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+
+{
+    class Program
+    {
+        static int Find(int value, int[] arr)
+        {
+            int i = 0;
+            while (arr[i] != value)
+            {
+            if (++i > arr.Length)
+            throw new ArgumentException();
+            }
+            return i;
+        }
+        static void Main()
+        {
+            Console.WriteLine(Find(3, new int[] {5, 4, 3, 2, 1 }));
+        }
+    }
+}
